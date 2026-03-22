706e298daec95df2aa7f84b552c1488e79f01c17
Carolina Mulet <Carolina.Mulet1@alu.uclm.es>
2026-03-22 17:03:05 +0100

    Fixed package.R and Rdata

--- R/package.R
+++ R/package.R
@@ -8,11 +8,10 @@
 #'
 #' @name MissingBVS-package
 #' @aliases MissingBVS-package MissingBVS
-#' @docType package
 #' @author Carolina Mulet, Gonzalo Garcia-Donato and María Eugenia Castellanos
 #'
 #' Maintainer: Carolina Mulet \email{Carolina.Mulet1@@alu.uclm.es}
-#' @seealso \code{\link[MissingBVS]{missingBtest.lm}}, 
+#' @seealso \code{\link[MissingBVS]{missingBtest.lm}},
 #' \code{\link[MissingBVS]{missingBvs.lm}},
 #' \code{\link[MissingBVSl]{missingGibbsBvs.lm}}
 #' @references
@@ -23,10 +22,10 @@
 #' Bayarri, M.J., Berger, J.O., Forte, A. and Garcia-Donato, G.
 #' (2012)<DOI:10.1214/12-aos1013> Criteria for Bayesian Model choice with
 #' Application to Variable Selection. The Annals of Statistics. 40: 1550-1577
-#' 
+#'
 #' van Buuren, S. and Groothuis-Oudshoorn, K. (2011) mice: Multivariate Imputation
 #' by Chained Equations in R. Journal of Statistical Software. 45: 1–67.
-#' 
+#'
 #' Clyde, M (2025) BAS: Bayesian Variable Selection and Model Averaging using
 #' Bayesian Adaptive Sampling. R package version 2.0.2
 #' <https://CRAN.R-project.org/package=BAS>.
@@ -34,11 +33,11 @@
 #' Fernandez, C., Ley, E. and Steel, M.F.J.
 #' (2001)<DOI:10.1016/s0304-4076(00)00076-2> Benchmark priors for Bayesian
 #' model averaging. Journal of Econometrics, 100, 381-427.
-#' 
+#'
 #' García-Donato, G., Castellanos, M.E., Cabras, S., Quirós, A.
 #' and Forte, A. (2025) Model Uncertainty and Missing Data: An Objective Bayesian
 #' Perspective (with Discussion). Bayesian Analysis. 20: 1677–1778.
-#' 
+#'
 #' García-Donato, G. and Forte, A. (2018) Bayesian Testing,
 #' Variable Selection and Model Averaging in Linear Models using R with
 #' BayesVarSel. The R Journal. 10: 329.
@@ -47,12 +46,12 @@
 #' (2013)<DOI:10.1080/01621459.2012.742443> On sampling strategies in Bayesian
 #' variable selection problems with large model spaces. Journal of the American
 #' Statistical Association. 108: 340-352.
-#' 
+#'
 #' Held, L., Gravestock, I. and Sabanés Bové, D.
 #' (2015)<DOI:10.1080/01621459.2014.993077> Objective Bayesian model selection
 #' for generalized linear models using test-based Bayes factors. Journal of the
 #' American Statistical Association, 110, 1157–1168.
-#' 
+#'
 #' Li, Y. and Clyde, M. (2018)<DOI:10.1080/01621459.2018.1469992> Mixtures
 #' of g-Priors in Generalized Linear Models. Journal of the American
 #' Statistical Association. 113: 1275–1287.
@@ -61,10 +60,10 @@
 #' (2008)<DOI:10.1198/016214507000001337> Mixtures of g-priors for Bayesian
 #' Variable Selection. Journal of the American Statistical Association.
 #' 103:410-423.
-#' 
+#'
 #' Schwarz, G. (1978) Estimating the dimension of a model. The Annals of
 #' Statistics. 6: 461–464.
-#' 
+#'
 #' Scott, J.G. and Berger, J.O. (2010) Bayes and empirical-Bayes multiplicity
 #' adjustment in the variable-selection problem. The Annals of Statistics.
 #' 38: 2587–2619.
@@ -81,20 +80,20 @@
 #' Bayesian Regression Analysis with g-prior Distributions. In Bayesian
 #' Inference and Decision techniques: Essays in Honor of Bruno de Finetti (A.
 #' Zellner, ed.) 389-399. Edward Elgar Publishing Limited.
-#' 
+#'
 #' @keywords package
 #' @examples
 #' # To be completed
 #'
-NULL
+"_PACKAGE"
 
 
 #' Sala-i-Martin 97 data
 #'
 #' Data set contains 63 variables potentially related with economic Growth (GDP).
-#' The dataset is in its original shape with many variables containing missing 
+#' The dataset is in its original shape with many variables containing missing
 #' observations coded as NA. The number of countries gathered is n=140.
-#' 
+#'
 #' It was firstly used in Sala-i-Martin (1997) and later analized by Fernández,
 #' Ley and Steel (2001) through a BVS perspective deleting missing obeservations.
 #' Taken from the Journal of Data Archive - Journal of Applied Econometrics.
@@ -103,13 +102,13 @@ NULL
 #' @name dataS97
 #' @docType data
 #' @format A data frame with 140 observations and 63 variables
-#' \describe{ \item{\code{gr56092}}{Growth rate of GDP per capita between 1960 
+#' \describe{ \item{\code{gr56092}}{Growth rate of GDP per capita between 1960
 #' and 1992. Response variable.}
 #' \item{\code{Mining}}{Fraction of GDP in mining.}
 #' \item{\code{ECORG}}{Degree Capitalism index. Coded with 0,...,5 as follows:
 #' 0=statis; 1=mixed statist; 2=mixed capitalist-statist; 3=capitalist-statist;
 #' 4=mixed-capitalist; 5=capitalist.}
-#' \item{\code{YrsOpen}}{Number of years economy has been open between 1950 and 
+#' \item{\code{YrsOpen}}{Number of years economy has been open between 1950 and
 #' 1990.}
 #' \item{\code{EngFrac}}{Fraction of population speaking English.}
 #' \item{\code{OthFrac}}{Fraction of population speaking foreign language.}
@@ -122,7 +121,7 @@ NULL
 #' \item{\code{SCOUT}}{Measure of outward orientation.}
 #' \item{\code{STDC6089}}{Standard deviation of Gross Domestic Credit between
 #' 1960 and 1989.}
-#' \item{\code{STPI6089}}{Standard deviation of the inflation rate between 1960 
+#' \item{\code{STPI6089}}{Standard deviation of the inflation rate between 1960
 #' and 1990.}
 #' \item{\code{bmp1}}{Log of (1 + Black market premium).}
 #' \item{\code{area}}{(Scale effect) Total area of the country.}
@@ -130,22 +129,22 @@ NULL
 #' \item{\code{freetar}}{Degree of tariff barriers.}
 #' \item{\code{laam}}{Dummy for Latin American countries.}
 #' \item{\code{safrica}}{Dummy for Sub-Saharan African countries.}
-#' \item{\code{pyr60}}{Average years of primary schooling of total population 
+#' \item{\code{pyr60}}{Average years of primary schooling of total population
 #' in 1960.}
-#' \item{\code{syr60}}{Average years of secondary schooling of total population 
+#' \item{\code{syr60}}{Average years of secondary schooling of total population
 #' in 1960.}
-#' \item{\code{hyr60}}{Average years of higher education of total population 
+#' \item{\code{hyr60}}{Average years of higher education of total population
 #' in 1960.}
 #' \item{\code{human60}}{Average years of education of total population in 1960.}
 #' \item{\code{p60}}{Enrollment rate in primary education in 1960.}
 #' \item{\code{s60}}{Enrollment rate in secondary education in 1960.}
 #' \item{\code{h60}}{Enrollment rates in higher education in  1960.}
 #' \item{\code{lifee060}}{Life expectancy in 1960.}
-#' \item{\code{ggcfc3}}{Average share of expenditures on public investment as 
+#' \item{\code{ggcfc3}}{Average share of expenditures on public investment as
 #' fraction of GDP between 1960 and 1965.}
-#' \item{\code{geerec1}}{Average share public expenditures on education as 
+#' \item{\code{geerec1}}{Average share public expenditures on education as
 #' fraction of GDP between 1960 and 1965.}
-#' \item{\code{gde1}}{Average share public expenditures on defense as fraction 
+#' \item{\code{gde1}}{Average share public expenditures on defense as fraction
 #' of GDP between 1960 and 1965.}
 #' \item{\code{assassp2}}{Number of political assassinations.}
 #' \item{\code{revcoup}}{Number of revolutions and military coups.}
@@ -175,21 +174,25 @@ NULL
 #' \item{\code{URB60}}{Fraction of population living in cities.}
 #' \item{\code{dpop6090}}{Average growth rate of population between 1960 and 1990.}
 #' \item{\code{gdpsh60l}}{Logarithm of GDP per capita in 1960.}
-#' \item{\code{humanyl}}{Product of average years of schooling and log of GDP 
+#' \item{\code{humanyl}}{Product of average years of schooling and log of GDP
 #' per capita in 1960.}
 #' \item{\code{lly1}}{Ratio of liquid liabilities to GDP (a measure of financial
 #' development.)}
 #' \item{\code{work60l}}{Ratio workers to population.}
 #' \item{\code{lforce60}}{Size of labor force (scale effect).}
-#' \item{\code{gvxdxe52}}{Public consumption minus education and defense as 
+#' \item{\code{gvxdxe52}}{Public consumption minus education and defense as
 #' fraction of GDP.}
 #' }
-#' 
-#' @references Sala i Martin, X. (1997)
-#' I Just Ran Two Million Regressions. The American Economic Review, 87(2), 178–183.
-#'  
+#'
+#' @references Sala i Martin, X. (1997) I Just Ran Two Million Regressions.
+#' The American Economic Review, 87(2), 178–183.
+#'
+#' Fernández, C., Ley, E., and Steel., M.F.J. (2001). Model Uncertainty
+#' in Cross-Country Growth Regressions. Journal of Applied Econometrics,
+#' 16(5), 563–576.
+#'
 #' @keywords datasets
-#' 
+#'
 #' @examples
 #' data(dataS97)
 #'
@@ -198,19 +201,19 @@ NULL
 
 #' OBICE data
 #'
-#' Dataset corresponding to the OBICE study (Zurriaga et al 2011) where factors 
-#' associated with childhood obesity are studied. The data were collected in 
-#' 2007 and 2008 through several questionnaries and n=1188 children were enroled 
+#' Dataset corresponding to the OBICE study (Zurriaga et al 2011) where factors
+#' associated with childhood obesity are studied. The data were collected in
+#' 2007 and 2008 through several questionnaries and n=1188 children were enroled
 #' in the study. It contains 155 variables.
-#' 
-#' This is a case and control study with 437 cases (obese) and 751 controls 
+#'
+#' This is a case and control study with 437 cases (obese) and 751 controls
 #' (not obese). Purposedly the dataset is distributed without any post-processing
 #' and therefore, many variables contain missing observations coded in different way.
 #'
 #'
 #' @name OBICE
 #' @docType data
-#' @format A data frame with 1188 entries and 121 variables. The more relevant 
+#' @format A data frame with 1188 entries and 121 variables. The more relevant
 #' are described. Contact us if you need specific information of any other.
 #' \describe{
 #' \item{Acostarse}{does he/she eat before going to bed? (yes/no) }
@@ -341,7 +344,9 @@ NULL
 #' Factors associated with childhood obesity in Spain. The OBICE
 #' study: a case–control study based on sentinel networks. Public Health Nutrition
 #' 14(6), 1105–1113.
+#'
 #' @keywords datasets
+#'
 #' @examples
 #' data(OBICE)
 #'
